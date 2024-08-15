5b3fa82e38ed7cc57508607acb0a90077d911d15
Sina <sinasamii7878@gmail.com>
2024-08-15 22:02:21 +0330

    Add disable attribute to handle badges

--- todo-app/src/components/Button.jsx
+++ todo-app/src/components/Button.jsx
@@ -1,9 +1,16 @@
-function Button({ buttonType, inputValue, buttonOnClick, buttonStyle }) {
+function Button({
+  buttonType,
+  inputValue,
+  buttonOnClick,
+  disabled,
+  buttonStyle,
+}) {
   return (
     <input
       type={buttonType}
       value={inputValue}
       onClick={buttonOnClick}
+      disabled={disabled}
       className={buttonStyle}
     />
   );
