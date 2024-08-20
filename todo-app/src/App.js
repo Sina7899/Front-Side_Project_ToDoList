64a9a2998ba1821ccd98c6391afd54622c3a5b2f
Sina <sinasamii7878@gmail.com>
2024-08-20 11:13:40 +0330

    Define app pages routes

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,1 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { LoginPage } from "./pages/Login.jsx";
+import { SignUp } from "./pages/Signup.jsx";
+import { ToDoApp } from "./pages/ToDoApp.jsx";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Login" />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignUp />} />
+        <Route path="/ToDoApp" element={<ToDoApp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
