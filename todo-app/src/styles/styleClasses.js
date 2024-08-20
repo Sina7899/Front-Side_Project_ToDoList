5a8381b70538b47847d64dda5a8f9a6593910737
Sina <sinasamii7878@gmail.com>
2024-08-20 11:12:54 +0330

    Add all pages base structure styles

---
+++ todo-app/src/styles/styleClasses.js
@@ -0,0 +1,71 @@
+export const CLASSES = {
+  Login_Page: {
+    mainTagStyle:
+      "flex justify-center items-center w-auto h-screen  bg-custom-image bg-cover font-roboto  p-[65px]",
+    formStyle:
+      "flex flex-col items-center p-[42px_32px] h-fit max-w-[472px] gap-[32px] rounded-[5px] bg-Blue_B5",
+    formTitleStyle:
+      "self-stretch text-center text-White_W3 text-[36px] font-IBM font-bold leading-normal drop-shadow-xl",
+    loginSection: "flex justify-center items-end gap-[10px]",
+    fieldsetStyle: "border-none flex gap-[10px]",
+    labelStyle: "text-Black_B7 text-[12px] font-bold block text-left",
+    inputStyle:
+      "block w-[160px] h-[28px] border-none p-[4px] mt-[6px] rounded-[5px] placeholder:text-Black_B5 placeholder:text-[10px]",
+    loginButtonStyle:
+      "w-[68px] h-[28px] rounded-[5px] bg-Blue_B6 text-[10px] font-bold text-White_W3 text-center p-0 hover:cursor-pointer hover:bg-Blue_B7",
+    signUpButtonStyle:
+      "w-[68px] h-[28px] rounded-[5px] bg-Black_B7 text-[10px] font-bold text-White_W3 text-center p-0 hover:cursor-pointer hover:bg-Black_B6",
+  },
+  SignUp_Page: {
+    mainTagStyle:
+      "flex justify-center items-center w-auto h-screen  bg-custom-image bg-cover font-roboto  p-[65px]",
+    formStyle:
+      "flex flex-col items-center p-[42px_38px] h-fit w-fit max-w-[328px] gap-[40px] rounded-[5px] bg-Blue_B5",
+    formTitleStyle:
+      "self-stretch text-center text-White_W3 text-[36px] font-IBM font-bold leading-normal drop-shadow-xl",
+    fieldsetStyle: "border-none flex flex-col gap-[10px]",
+    labelStyle: "text-Black_B7 text-[12px] font-bold block text-left",
+    inputStyle:
+      "block w-[160px] h-[28px] border-none p-[4px] mt-[6px] rounded-[5px] placeholder:text-Black_B5 placeholder:text-[10px]",
+    signInButtonStyle:
+      "w-[68px] h-[28px] rounded-[5px] bg-Black_B7 text-[10px] font-bold text-White_W3 text-center p-0 hover:cursor-pointer hover:bg-Black_B6",
+  },
+  ToDoApp_Page: {
+    mainTagStyle:
+      "flex justify-between items-start gap-[64px] w-screen h-screen bg-White_W3 font-roboto p-[76px_64px]",
+    navStyle: "flex flex-col items-center gap-[118px] h-fit w-auto",
+    navHeaderStyle: "flex flex-col justify-center items-center gap-[14px]",
+    avatarStyle: "w-[96px] h-[96px] object-cover rounded-[96px]",
+    userNameStyle: "text-Black_B7 text-[12px] font-bold",
+    navListItemsTagStyle: "flex flex-col justify-center items-start gap-[10px]",
+    listItemsStyle:
+      "flex justify-center items-center gap-[8px] p-[2px_8px] bg-Blue_B5 rounded-[5px]",
+    iconStyle: "w-[28px] h-[28px] text-Black_B7",
+    spanStyle: "text-Black_B7 text-[16px] font-medium",
+    cardsSectionStyle:
+      "flex flex-col justify-start items-center gap-[20px] overflow-y-scroll h-fit max-h-full w-[560px] p-[28px_20px] bg-Blue_B3 rounded-[10px] custom-scrollbar",
+    cardTagStyle:
+      "flex justify-between items-center w-[468px] min-h-[90px] p-[0px_20px] bg-White_W3 rounded-[5px]",
+    cardInfoDivStyle: "flex flex-col justify-center items-start gap-[8px]",
+    cardTitleTagStyle: "text-Black_B7 text-[20px] font-bold",
+    cardDescriptionStyle: "text-Black_B7 text-[14px] font-normal",
+    cardButtonsDivStyle: "flex flex-col justify-center items-center gap-[4px]",
+    doneButtonStyle:
+      "w-[68px] h-[28px] rounded-[5px] bg-Blue_B6 text-[10px] font-bold text-White_W3 text-center p-0 hover:cursor-pointer hover:bg-Blue_B7",
+    deleteButtonStyle:
+      "w-[68px] h-[28px] rounded-[5px] bg-Black_B7 text-[10px] font-bold text-White_W3 text-center p-0 hover:cursor-pointer hover:bg-Black_B6",
+    completedBadgeStyle:
+      "w-[68px] h-[24px] rounded-[5px] bg-Blue_B7 text-[10px] font-bold text-White_W3 text-center p-0",
+    boxContainerTagStyle:
+      "flex w-[544px] p-[28px] flex-col justify-center items-end gap-[24px] rounded-[10px] border border-dashed border-Black_B7",
+    boxDataDivStyle: "flex flex-col justify-start items-start gap-[8px] w-full",
+    boxTitleInputStyle:
+      "placeholder:text-[20px] placeholder:font-bold placeholder:text-Black_B6 text-[20px] text-Black_B7 font-bold text-start w-100 focus:outline-none",
+    boxDescriptionTextareaStyle:
+      "min-h-[220px] w-full p-[10px] self-stretch rounded-[5px] border border-[#666666] border-[0.5px] custom-scrollbar",
+    boxButtonsDivStyle: "flex justify-center items-center gap-[8px]",
+    boxAddButtonStyle:
+      "flex justify-center items-center gap-[4px] w-[52px] h-[28px] rounded-[5px] bg-Blue_B6 hover:bg-Blue_B7",
+    boxAddButtonSpanStyle: "text-[10px] font-bold text-White_W3",
+  },
+};
