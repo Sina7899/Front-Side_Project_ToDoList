218d686c83fb3172b5d69f37b7599acf0c8ff95c
Sina Sami <sinasamii7878@gmail.com>
2024-08-28 14:37:44 +0330

    Move/Add CRUD-api handlers

--- todo-app/src/API/todoApp.js
+++ todo-app/src/API/todoApp.js
@@ -1,5 +1,7 @@
 import { get, post, put, Delete } from "./api.js";
 
+import { addToLocalStorage } from "../local-Storage.js";
+
 function getTasksRequest(url, params, token) {
   const response = get(url, params, token);
   return response;
@@ -20,9 +22,111 @@ function deleteTaskRequest(url, data, token) {
   return response;
 }
 
+function getTasksHandler(url, params, token) {
+  return getTasksRequest(url, params, token)
+    .then((response) => {
+      if (response.status === 200) {
+        console.log("Request was successful:", response.data);
+        let responseData = {
+          tasks: response.data.tasks,
+          userInfo: response.data.userInfo,
+        };
+        addToLocalStorage("firstName", response.data.userInfo.firstName);
+        addToLocalStorage("lastName", response.data.userInfo.lastName);
+        return responseData;
+      } else {
+        console.log("Request was not successful:", response);
+        if (
+          response.request.response === "" ||
+          response.request.response === undefined
+        ) {
+          alert("Timeout, Please Try Again");
+        } else {
+          alert(response.request.response);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("There was an error making the request:", error);
+    });
+}
+
+function addTaskHandler(url, data, token) {
+  return addTaskRequest(url, data, token)
+    .then((response) => {
+      if (response.status === 201) {
+        console.log("Request was successful:", response.data);
+        return response.data;
+      } else {
+        console.log("Request was not successful:", response);
+        if (
+          response.request.response === "" ||
+          response.request.response === undefined
+        ) {
+          alert("Timeout, Please Try Again");
+        } else {
+          alert(response.request.response);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("There was an error making the request:", error);
+    });
+}
+
+function editTaskHandler(url, data, token) {
+  return updateTaskRequest(url, data, token)
+    .then((response) => {
+      if (response.status === 201) {
+        console.log("Request was successful:", response.data);
+        return response.data;
+      } else {
+        console.log("Request was not successful:", response);
+        if (
+          response.request.response === "" ||
+          response.request.response === undefined
+        ) {
+          alert("Timeout, Please Try Again");
+        } else {
+          alert(response.request.response);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("There was an error making the request:", error);
+    });
+}
+
+function deleteTaskHandler(url, data, token) {
+  return deleteTaskRequest(url, data, token)
+    .then((response) => {
+      if (response.status === 200) {
+        console.log("Request was successful:", response.data);
+        return response.data;
+      } else {
+        console.log("Request was not successful:", response);
+        if (
+          response.request.response === "" ||
+          response.request.response === undefined
+        ) {
+          alert("Timeout, Please Try Again");
+        } else {
+          alert(response.request.response);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("There was an error making the request:", error);
+    });
+}
+
 export {
   getTasksRequest,
   addTaskRequest,
   updateTaskRequest,
   deleteTaskRequest,
+  getTasksHandler,
+  addTaskHandler,
+  editTaskHandler,
+  deleteTaskHandler,
 };
